660cc1166f4edd4d80a96217285f128ae98b22b7
youva-cn <diyouva@gmail.com>
2024-06-28 14:16:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -88,25 +88,30 @@ def main ():
   # You can also use "with" notation:
   with tab1:
      st.radio("Select one:", [1, 2])
+ 
+ #Show a spinner during a process
+  with st.spinner(text="In progress"):
+    time.sleep(3)
+    st.success("Done")
 
-  # Show and update progress bar
-  bar = st.progress(50)
-  time.sleep(3)
-  bar.progress(100)
-  
-  with st.status("Authenticating...") as s:
-      time.sleep(2)
-      st.write("Some long response.")
-      s.update(label="Response")
-  
-  st.balloons()
-  st.snow()
-  st.toast("Warming up...")
-  st.error("Error message")
-  st.warning("Warning message")
-  st.info("Info message")
-  st.success("Success message")
-  st.exception(e)
+ #Show and update progress bar
+ bar = st.progress(50)
+ time.sleep(3)
+ bar.progress(100)
+
+ with st.status("Authenticating...") as s:
+    time.sleep(2)
+    st.write("Some long response.")
+    s.update(label="Response")
+
+ st.balloons()
+ st.snow()
+ st.toast("Warming up...")
+ st.error("Error message")
+ st.warning("Warning message")
+ st.info("Info message")
+ st.success("Success message")
+ st.exception(e)
 
 if __name__ == '__main__' :
   main()
