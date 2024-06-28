c408b9db5f6d000ba195cd350aeb384b28af5516
youva-cn <diyouva@gmail.com>
2024-06-28 13:51:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,22 @@ def main ():
   sidebar_checkbox = st.sidebar.checkbox('Checkbox di Sidebar')
   sidebar_radio_button = st.sidebar.radio('Pilih Menu',options=['A','B','C'])
 
+  #columns :
+  col1, col2, col3 = st.columns([1, 2, 1])
+
+  with col1:
+      st.header("A cat")
+      st.image("https://static.streamlit.io/examples/cat.jpg")
+  #atau dengan assignment 
+  image_col1 = col1.image("https://static.streamlit.io/examples/cat.jpg")
+
+  with col2:
+      st.header("A dog")
+      st.image("https://static.streamlit.io/examples/dog.jpg")
+
+  with col3:
+      st.header("An owl")
+      st.image("https://static.streamlit.io/examples/owl.jpg")
+
 if __name__ == '__main__' :
   main()
