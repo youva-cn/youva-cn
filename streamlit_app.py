64864b98e6c2c8d0358293eeece305cfdcc60200
youva-cn <diyouva@gmail.com>
2024-06-28 13:31:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,17 @@ def main ():
   # Menampilkan AgGrid
   st.write('Menampilkan Dataframe dengan St AgGrid')
   AgGrid(house)
+  
   st.table([x for x in range(1,5)])
+  
+  click_me_btn = st.button('Click Me')
+  st.write(click_me_btn) #Return True kalo di Click 
+  check_btn = st.checkbox('Klik Jika Setuju')
+  if check_btn :
+      st.write('Anda Setuju')
+  
+  radio_button= st.radio('Choose below',[x for x in range(1,3)])
+  st.write('Anda Memilih',radio_button)
+
 if __name__ == '__main__' :
   main()
