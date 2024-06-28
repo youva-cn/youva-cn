5c5b867dbafc3267a1261166cc251ce0db725440
youva-cn <diyouva@gmail.com>
2024-06-28 11:33:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,6 @@ def main ():
   st.metric(label="Temperature", value="70 °F", delta="-1.2 °F")
   st.write('Menampilkan Dataframe dengan St AgGrid')
   AgGrid(house)
+  st.table([x for x in range(1,5)])
 if __name__ == '__main__' :
   main()
