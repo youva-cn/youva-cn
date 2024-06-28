6e8460e21d515e75d9f066d871993f43c3d9d6c0
youva-cn <diyouva@gmail.com>
2024-06-28 14:12:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,5 +80,33 @@ def main ():
 
   st.write("Outside the form")
 
+  # Insert containers separated into tabs:
+  tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+  tab1.write("this is tab 1")
+  tab2.write("this is tab 2")
+
+  # You can also use "with" notation:
+  with tab1:
+     st.radio("Select one:", [1, 2])
+
+  # Show and update progress bar
+  bar = st.progress(50)
+  time.sleep(3)
+  bar.progress(100)
+  
+  with st.status("Authenticating...") as s:
+      time.sleep(2)
+      st.write("Some long response.")
+      s.update(label="Response")
+  
+  st.balloons()
+  st.snow()
+  st.toast("Warming up...")
+  st.error("Error message")
+  st.warning("Warning message")
+  st.info("Info message")
+  st.success("Success message")
+  st.exception(e)
+
 if __name__ == '__main__' :
   main()
