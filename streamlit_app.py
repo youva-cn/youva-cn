07ed63bb25901eb248bd15d3edef39de94fb3a15
youva-cn <diyouva@gmail.com>
2024-06-28 13:49:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,7 @@ def main ():
   
   st.table([x for x in range(1,5)])
   
+
   click_me_btn = st.button('Click Me')
   st.write(click_me_btn) #Return True kalo di Click 
   check_btn = st.checkbox('Klik Jika Setuju')
@@ -31,16 +32,16 @@ def main ():
   
   radio_button= st.radio('Choose below',[x for x in range(1,3)])
   st.write('Anda Memilih',radio_button)
-
-  #slider
+    
+  #slider 
   age_slider = st.slider('Berapa Usia Anda',0,100)
   st.write('Usia Anda',age_slider)
-
-  # Menggunakan Input (Typing)
+    
+  #Input (Typing)
   num_input = st.number_input('Input Berapapun')
   st.write('Kuadrat dari {} adalah {}'.format(num_input,num_input**2))
 
-  # menampilkan sidebar 
+  ##### Menampilkan menu sidebar 
   sidebar_checkbox = st.sidebar.checkbox('Checkbox di Sidebar')
   sidebar_radio_button = st.sidebar.radio('Pilih Menu',options=['A','B','C'])
 
